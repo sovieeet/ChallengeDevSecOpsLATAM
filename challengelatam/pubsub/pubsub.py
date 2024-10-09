3e96311f13fa66c610024f68efdf0019f59fef9d
Manuel Barrera <manubarrerazurita@gmail.com>
2024-10-09 20:21:39 -0300

    FIX: specified project_id in pubsub.py

--- challengelatam/pubsub/pubsub.py
+++ challengelatam/pubsub/pubsub.py
@@ -7,7 +7,7 @@ DATASET_ID = "desafio_latam"
 TABLE_ID = "latam"
 
 def pubsub_to_bigquery(event, context):
-    client = bigquery.Client()
+    client = bigquery.Client(project=PROJECT_ID)
     table_id = f"{PROJECT_ID}.{DATASET_ID}.{TABLE_ID}"
 
     message = base64.b64decode(event['data']).decode('utf-8')
