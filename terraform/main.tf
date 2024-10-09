4419f8731c5c41c8a77b7a0880651e341e513b28
Manuel Barrera <manubarrerazurita@gmail.com>
2024-10-09 17:10:51 -0300

    fix: error when the resource is created

--- terraform/main.tf
+++ terraform/main.tf
@@ -9,6 +9,10 @@ resource "google_project_iam_member" "pubsub_admin" {
   project = var.project_id
   role    = "roles/pubsub.admin"
   member  = "serviceAccount:${var.google_service_account_email}"
+
+  lifecycle {
+    ignore_changes = [role, member]
+  }
 }
 
 # BigQuery Admin
@@ -16,6 +20,10 @@ resource "google_project_iam_member" "bigquery_admin" {
   project = var.project_id
   role    = "roles/bigquery.admin"
   member  = "serviceAccount:${var.google_service_account_email}"
+
+  lifecycle {
+    ignore_changes = [role, member]
+  }
 }
 
 # Storage Admin
@@ -23,13 +31,20 @@ resource "google_project_iam_member" "storage_admin" {
   project = var.project_id
   role    = "roles/storage.admin"
   member  = "serviceAccount:${var.google_service_account_email}"
-}
 
+  lifecycle {
+    ignore_changes = [role, member]
+  }
+}
 
 # BigQuery Dataset
 resource "google_bigquery_dataset" "desafio_latam" {
   dataset_id = var.dataset_id
   location   = var.location
+
+  lifecycle {
+    prevent_destroy = true
+  }
 }
 
 # BigQuery Table
@@ -42,9 +57,13 @@ resource "google_bigquery_table" "latam1" {
 # Pub/Sub Topic
 resource "google_pubsub_topic" "data_ingestion" {
   name = "data-ingestion"
+
+  lifecycle {
+    prevent_destroy = true
+  }
 }
 
-# Cloud Storage Bucket to storage the function code
+# Cloud Storage Bucket to store the function code
 resource "google_storage_bucket" "function_bucket" {
   name     = "${var.project_id}-function-bucket"
   location = var.region
@@ -57,19 +76,19 @@ resource "google_storage_bucket_object" "function_code" {
   source = "${path.module}/function.zip"
 }
 
-# Cloud Function activated with Pub/Sub and write in BigQuery
+# Cloud Function activated with Pub/Sub and writes to BigQuery
 resource "google_cloudfunctions_function" "pubsub_to_bigquery" {
   name        = "pubsub-to-bigquery"
   description = "Function to insert Pub/Sub data into BigQuery"
   runtime     = "python39"
   region      = var.region
 
-  # Function setting
+  # Function settings
   entry_point           = "pubsub_to_bigquery"
   source_archive_bucket = google_storage_bucket.function_bucket.name
   source_archive_object = google_storage_bucket_object.function_code.name
 
-  # Trigger setting
+  # Trigger settings
   event_trigger {
     event_type = "google.pubsub.topic.publish"
     resource   = google_pubsub_topic.data_ingestion.id
